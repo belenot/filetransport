d0e158b0168068751d9180dc17ddabb501d80414
belenot <kholhunov0@gmail.com>
2019-03-24 04:55:55 +0300

    Refactoring: ClientService now provide only socket process and hold Function<T,R>, where T - ClientQuery and R - ServerResponse

--- src/belenot/filetransport/ClientQuery.java
+++ src/belenot/filetransport/ClientQuery.java
@@ -8,34 +8,9 @@ public class ClientQuery {
 	
 	public ClientQuery(InputStream in)
 		throws IllegalArgumentException, IOException {
-		byte[] bytes = new byte[in.available()];
-		in.read(bytes);
-	    List<String> fields = new LinkedList<>();
-		String field = "";
-		for (byte b : bytes) {
-			if(b == 0) {
-				if(field.equals(""))
-					throw new IllegalArgumentException("Wrong input data, expected field");
-				fields.add(field);
-				field = "";
-			} else {
-				field += (char) b;
-			}
-		}
-		fields.add(field);
-		System.out.println(fields);
-		if(fields.size() == 0)
-			throw new IllegalArgumentException("Null client query");
-		if(fields.size() % 2 == 0)
-			throw new IllegalArgumentException("Incorrect client query, expected value");
-		try {
-			clientCommand = ClientCommand.valueOf(fields.get(0));
-		} catch (IllegalArgumentException exc) {
-			throw new IllegalArgumentException("Incorrect client command");
-		}
-		for(int i = 1; i + 1 < fields.size(); i++) {
-			data.put(fields.get(i), fields.get(i + 1));
-		}	
+		//Сделать парсинг потока
+			
+			
 			
 	}
 
@@ -44,6 +19,6 @@ public class ClientQuery {
 	public Map<String, String> getData() { return data; }
 
 	private ClientCommand clientCommand;
-	private Map<String, String> data = new HashMap<>();
+	private Map<String, int[]> data = new HashMap<>();
 }
 		
