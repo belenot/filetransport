2e063087e3436356ee1d2c7e8d5e391b91c429f0
belenot <kholhunov0@gmail.com>
2019-03-24 11:15:13 +0300

    Try to use serialization on ClientQuery

--- src/belenot/filetransport/ClientQuery.java
+++ src/belenot/filetransport/ClientQuery.java
@@ -6,44 +6,18 @@ import java.util.function.*;
 
 public class ClientQuery {
 	
-	public ClientQuery(InputStream in)
-		throws IllegalArgumentException, IOException {
-		byte[] buffer = new byte[256];
-		int b = 0;
-		int i = 0;
-		while( (b = in.read()) != -1 && b != 0) {
-			buffer[i++] = (byte) b;
-		}
-		try {
-			clientCommand = ClientCommand.valueOf(new String(buffer, 0, i));
-		} catch (IllegalArgumentException exc) {
-			throw new IllegalArgumentException("Incorrect command: " + new String(buffer));
-		}
-		boolean stop = false;
-		while(!stop) {
-			buffer = new byte[256];
-			i = 0;
-			while( (b = in.read()) != -1 && b != 0) {
-				buffer[i++] = (byte) b;
-			}
-			if (b == -1)break;
-			String key = new String(buffer, 0, i);
-			buffer = new byte[256];
-			i = 0;
-			while( (b = in.read()) != -1 && b != 0) {
-				buffer[i++] = (byte) b;
-			}
-			byte[] value = Arrays.copyOf(buffer, i);
-			data.put(key, value);
-			if (b == -1)break;
-		}
+	public ClientQuery() {
+		
 	}
+	
 
 	public ClientCommand getClientCommand() { return clientCommand; }
 
-	public Map<String, byte[]> getData() { return data; }
+	public Map<String, byte[]> getHeaders() { return headers; }
+	public byte[] data getData() { return data; }
 
 	private ClientCommand clientCommand;
-	private Map<String, byte[]> data = new HashMap<>();
+	private Map<String, String> headers = new HashMap<>();
+	private byte[] data;
 }
 		
