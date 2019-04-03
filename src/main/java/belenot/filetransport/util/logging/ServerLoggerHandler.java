5fc5ce505af93900d1a876cf6b6baa6ee8dcdd4a
belenot <kholhunov0@gmail.com>
2019-04-03 18:35:44 +0300

    Created stubs for ServerLogger and ServerLoggerHandler

---
+++ src/main/java/belenot/filetransport/util/logging/ServerLoggerHandler.java
@@ -0,0 +1,44 @@
+package belenot.filetransport.util.logging;
+
+import java.util.logging.*;
+import java.io.*;
+
+public class ServerLoggerHandler extends Handler {
+	private OutputStream out = System.err;
+
+    public ServerLoggerHandler() {
+		super();
+	}
+
+	@Override
+	public void close() throws SecurityException {
+		if(out != System.err) {
+			try {
+				out.close();
+			}
+			catch (IOException exc) {
+				/*What I shall do?*/
+			}
+		}
+	}
+
+	@Override
+	public void flush() throws SecurityException {
+		try {
+		if(out instanceof Flushable) out.flush();
+		}
+		catch (IOException exc) {
+			/*What I shall do?*/
+		}
+	}
+
+	@Override
+	public void publish(LogRecord record) {
+		try {
+			out.write( (record.getMessage() + "\n").getBytes());
+		}
+		catch (IOException exc) {
+			/*What I shall do?*/
+		}
+	}
+}
